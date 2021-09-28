34f1e7deb74598e59b637912ca41e996cf9f1883
yampy <gstyley@gmail.com>
2021-09-28 23:08:15 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Amplify!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App, {includeGreetings: true})
